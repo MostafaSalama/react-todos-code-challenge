d655fe5076f62c68359b69d17f110f422f53d730
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 00:44:52 +0200

    add delete todo handler in TodoItem

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-
 import styles from './styles.module.sass';
 import {CompleteButton, DeleteButton} from "../common/Button";
+import {useDispatch} from "react-redux";
+import {removeTodo} from "../../redux/action-creators";
 
 function TodoItem({todo}) {
+  const dispatch = useDispatch();
+  function deleteTodo() {
+    dispatch(removeTodo(todo.id));
+  }
   return (
     <div className={styles.card}>
       <h2 className={styles.title}>{todo.text}</h2>
       <CompleteButton onClick={null}>Complete</CompleteButton>
-      <DeleteButton onClick={null}>Delete</DeleteButton>
+      <DeleteButton onClick={deleteTodo}>Delete</DeleteButton>
     </div>
   );
 }
