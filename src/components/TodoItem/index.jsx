ecf12bbc009e3531227d485e5a2cc11217ed01c4
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 01:40:04 +0200

    add completeclass to the complete todo in TODOITEM

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styles from './styles.module.sass';
 import {CompleteButton, DeleteButton} from "../common/Button";
 import {useDispatch} from "react-redux";
-import {removeTodo} from "../../redux/action-creators";
+import {completeTODO, removeTodo} from "../../redux/action-creators";
 
 function TodoItem({todo}) {
   const dispatch = useDispatch();
+  // add completed class if the todo is completed
+  const completedClass = todo.completed ? styles.completed : '' ;
   function deleteTodo() {
     dispatch(removeTodo(todo.id));
   }
+  function completeTodo() {
+    dispatch(completeTODO(todo.id));
+  }
   return (
     <div className={styles.card}>
-      <h2 className={styles.title}>{todo.text}</h2>
-      <CompleteButton onClick={null}>Complete</CompleteButton>
+      <h2 className={`${styles.title} ${completedClass}`}>{todo.text}</h2>
+      {!todo.completed &&  <CompleteButton onClick={completeTodo}>Complete</CompleteButton>}
       <DeleteButton onClick={deleteTodo}>Delete</DeleteButton>
     </div>
   );
