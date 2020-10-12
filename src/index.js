f81caff1ff717976f510f8de11afc4a607ee0955
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-12 23:54:03 +0200

    add some global style for proper width setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexPage from './pages/index';
+import './index.css'
 import store from "./redux/store";
 import {Provider} from "react-redux";
 ReactDOM.render(
