4325b9c8a060193c47295466460386db0ca5988e
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 00:41:23 +0200

    create Button components to be used in other components

---
+++ src/components/common/Button/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react' ;
+import PropTypes from 'prop-types' ;
+import styles from './Button.module.scss';
+
+const Button = ({onClick,children,...props})=>{
+  return <button className={styles.btn} onClick={onClick} {...props}>{children}</button>
+}
+Button.propTypes = {
+  onClick : PropTypes.func.isRequired,
+  children:PropTypes.node
+
+}
+
+export const CompleteButton = ({onClick,children,...props})=>{
+  return <button className={styles.completeBtn} onClick={onClick} {...props}>{children}</button>
+}
+CompleteButton.propTypes = {
+  onClick : PropTypes.func.isRequired,
+  children:PropTypes.node
+}
+export const DeleteButton = ({onClick,children,...props})=>{
+  return <button className={styles.deleteBtn} onClick={onClick} {...props}>{children}</button>
+}
+CompleteButton.propTypes = {
+  onClick : PropTypes.func.isRequired,
+  children:PropTypes.node
+}
