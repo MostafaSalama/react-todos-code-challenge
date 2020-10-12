c3a9dbb39f35a7dc8ed4364a31187558192c3890
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-12 23:28:22 +0200

    make todoform dispatch to the store when the user submit the input

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import styles from './styles.module.sass';
-
+import {useDispatch} from "react-redux";
+import {addTodo} from "../../redux/action-creators";
+import {nanoid} from 'nanoid'
 function TodoForm() {
+  const [text,setText] = useState('');
+  const dispatch = useDispatch();
+
+  function onChangeHandler(e) {
+      const {value} = e.target ;
+      setText(value) ;
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    if(text) {
+      dispatch(addTodo({
+        text,
+        id:nanoid()
+      }));
+    }
+  }
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="What do you have to do?"
         className={styles.input}
+        onChange={onChangeHandler}
+        value={text}
       />
     </form>
   );
