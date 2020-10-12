dabcdbf3d9de65e057eb1dfb565ed2177aac2772
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 00:11:54 +0200

    replace the input component with the new one

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,36 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.sass';
-import {useDispatch} from "react-redux";
-import {addTodo} from "../../redux/action-creators";
-import {nanoid} from 'nanoid'
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../redux/action-creators';
+import { nanoid } from 'nanoid';
+import Input from '../common/Input';
 function TodoForm() {
-  const [text,setText] = useState('');
-  const dispatch = useDispatch();
+	const [text, setText] = useState('');
+	const dispatch = useDispatch();
 
-  function onChangeHandler(e) {
-      const {value} = e.target ;
-      setText(value) ;
-  }
-  function onSubmit(e) {
-    e.preventDefault();
-    if(text) {
-      dispatch(addTodo({
-        text,
-        id:nanoid()
-      }));
-    }
-  }
-  return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        placeholder="What do you have to do?"
-        className={styles.input}
-        onChange={onChangeHandler}
-        value={text}
-      />
-    </form>
-  );
+	function onChangeHandler(e) {
+		const { value } = e.target;
+		setText(value);
+	}
+	function onSubmit(e) {
+		e.preventDefault();
+		if (text) {
+			dispatch(
+				addTodo({
+					text,
+					id: nanoid(),
+				}),
+			);
+		}
+	}
+	return (
+		<form onSubmit={onSubmit}>
+			<Input
+				onChange={onChangeHandler}
+				value={text}
+				placeholder="What do you have to do?"
+			/>
+		</form>
+	);
 }
 
 export default TodoForm;
