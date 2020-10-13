51bd1d1613c8fbded7e19f7d0cf7b82ed7be50d5
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 03:18:14 +0200

    add ui to inProgress page

--- src/pages/InProgressTodos.jsx
+++ src/pages/InProgressTodos.jsx
@@ -1,6 +1,11 @@
 import React from 'react' ;
+import ContentContainer from "../components/ContentContainer";
+import TodoList from "../components/TodoList";
+import {getInProgressTodos} from "../util";
+import {useSelector} from "react-redux";
 
 const InProgressTodos = ()=>{
-  return <h1>InProgress Todos</h1>
+  const todos = useSelector(getInProgressTodos)
+  return <ContentContainer><TodoList todos={todos}/></ContentContainer>
 }
 export default  InProgressTodos ;
