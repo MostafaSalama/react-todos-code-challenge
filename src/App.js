c4040f085f0c01e1c616202ce34e29f94e321963
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 02:49:05 +0200

    create Route view for all pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react' ;
 import Header from "./components/Header";
+import {Route, Switch} from "react-router-dom";
+import IndexPage from "./pages";
+import AllTodos from "./pages/AllTodos";
+import CompletedTodos from "./pages/CompletedTodos";
+import InProgressTodos from "./pages/InProgressTodos";
 
 const App = ()=>{
   return (
-    <Header/>
+    <React.Fragment>
+      <Header/>
+       <Switch>
+         <Route path='/' component={IndexPage} exact/>
+         <Route path='/all' component={AllTodos} />
+         <Route path='/completed' component={CompletedTodos} />
+         <Route path='/in-progress' component={InProgressTodos} />
+       </Switch>
+    </React.Fragment>
   )
 }
 export default App ;
