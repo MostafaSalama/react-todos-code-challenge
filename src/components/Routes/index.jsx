500855ea12ebce156592af5367fe08ebe9d482bf
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-13 02:25:59 +0200

    update main Routes style

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import styles from './styles.module.sass'
 const Routes = ()=>{
   return (
    <nav>
-     <ul>
+     <ul className={styles.list}>
        <li><Link to='/all'>All</Link></li>
        <li><Link to='/completed'>Completed</Link></li>
        <li><Link to='/in-progress'>In Progress</Link></li>
